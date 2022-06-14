c92687fd308966eb9e8f62e0208caccc31a0bb40
Ashikur-CSE <ashikur201.cse@gmail.com>
2022-06-15 00:18:19 +0600

    15-06-2022

---
+++ 1294-A. Collecting Coins.py
@@ -0,0 +1,15 @@
+for i in range(int(input())):  
+    a,b,c,d=map(int,input().split())
+    x=max(a,b,c)
+    j=x-a
+    k=x-b
+    l=x-c
+    m=j+k+l
+    n=d-m
+    if n<0:
+        print("NO")
+    elif n%3==0:
+        print("YES")
+
+    else:
+        print("NO")
