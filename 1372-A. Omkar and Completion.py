bf6283ac64ddaea36003fb13e4e212ee4a7a4a76
Ashikur-CSE <ashikur201.cse@gmail.com>
2022-08-22 00:16:20 +0600

    21-08-2022

---
+++ 1372-A. Omkar and Completion.py
@@ -0,0 +1,18 @@
+for i in range (int(input())):
+    l1=[]
+    n=int(input())
+    if n<500:
+        for i in range(1, n+1):
+            x=(2 * i - 1)
+            l1.append(x)
+            s = [str(i) for i in l1] 
+            result = str(" ".join(s)) 
+        print(result)
+    elif n>500:
+        for i in range(1, 501):
+            x=(2 * i - 1)
+            l1.append(x)
+            x=(2*l1)
+            s = [str(i) for i in x[:n]] 
+            result = str(" ".join(s)) 
+        print(result)
