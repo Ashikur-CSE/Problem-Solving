99c0a22ec25cf792ec3d416ed74b59c5f1cfc64f
Ashikur-CSE <ashikur201.cse@gmail.com>
2022-09-01 13:26:11 +0600

    01-09-2022

--- 1650-A. Deletions of Two Adjacent Letters.py
+++ 1650-A. Deletions of Two Adjacent Letters.py
@@ -1,25 +1,9 @@
-# for i in range(int(input())):
-#     s=list(input())
-#     l=len(s)
-#     s2=input()
-#     n=(l-3)
-#     if l>3:
-#         for i in range(n):
-#             s.remove(s[0])
-#         if s2 in s and s[1]!=s2 :
-#             print("YES")
-#         elif s[0]==s2 and s[1]==s2 and s[2]==s2:
-#             print("YES")
-#         else:
-#             print("NO")
-#     else:
-#         if s2 in s and s[1]!=s2:
-#             print("YES")
-#         elif s[0]==s2 and s[1]==s2 and s[2]==s2:
-#             print("YES")
-#         else:
-#             print("NO")
-
-s = input()
-print(s[::2] )
+for i in range(int(input())):
+    s = input()
+    n=input()
+    l1=s[::2]
+    if n in l1:
+        print("Yes")
+    else:
+        print("NO")
     
